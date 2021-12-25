ef840de9a3ac8991c47c27333a628c898db23647
Ashutosh Ranjan <ashutosh.svea@gmail.com>
2021-12-25 19:44:32 +0100

    Added option for Console logging or file logging.

--- Ovning5/Program.cs
+++ Ovning5/Program.cs
@@ -4,17 +4,24 @@
 using Microsoft.Extensions.Configuration;
 using Ovning5;
 
-//try to learn the json file reading and dependency injection of logger to all relevant classes
-IConfiguration config = new ConfigurationBuilder()
-                    .SetBasePath(Environment.CurrentDirectory)
-                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true )
-                    .Build();
+ILogger logger = InitLog();
 
-string logFile = config.GetSection("Garage:LogFile").Value;
-//ILogger logger = new ConsoleLogger();
-ILogger logger = new FileLogger(Environment.CurrentDirectory + "\\" + logFile);
+ILogger InitLog()
+{
+    //try to learn the json file reading and dependency injection of logger to all relevant classes
+    IConfiguration config = new ConfigurationBuilder()
+                        .SetBasePath(Environment.CurrentDirectory)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .Build();
 
-Console.WriteLine("All log messages in: " + Environment.CurrentDirectory + "\\" + logFile);
+    string logFile = config.GetSection("Garage:LogFile").Value;
+    
+    switch (logFile)
+    {
+        case "Console" :    return new ConsoleLogger();
+        default:            return new FileLogger(Environment.CurrentDirectory + "\\" + logFile);
+    }
+}
 
 //Airplane airplane = new Airplane("XAM931", "SUV", "Silver", "Toyota", 4, 100);
 Airplane airplane = new Airplane(logger, "SAS1", "Jumbojet", "Silver", "Boeing", 5, 100);
@@ -22,9 +29,6 @@ Motorcycle motorcycle = new Motorcycle (logger, "m1", "Dirtbike", "Silver", "yam
 Car car = new Car (logger, "m1", "Dirtbike", "Silver", "yamaha", 4, 2);
 Bus bus = new Bus (logger, "B1", "Tourist", "Silver", "Volvo", 4, 20);
 Boat boat = new Boat (logger, "boat1", "Speedboat", "Black", "Sailor", 4, "Diesel");
-
-
-
 
 logger.log(airplane.PrintDetails());
 logger.log(car.PrintDetails());
